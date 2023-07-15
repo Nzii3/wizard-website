f027e649b4dee3253eb516bea0ad9990a97461d7
meymen <137542830+eymeter@users.noreply.github.com>
2023-07-15 09:02:48 +0300

    Update index.ts

--- components/index.ts
+++ components/index.ts
@@ -8,51 +8,6 @@ import Footer from "./Footer";
 import Team from "./about/Team";
 import Card from "./premium/Card";
 import chokidar from 'chokidar';
-import fs from 'fs';
-import path from 'path';
-
-const docsDir = path.resolve(__dirname, '../docs');
-const pagesDir = path.resolve(__dirname, '../pages');
-
-// just an extra check
-if (!fs.existsSync(pagesDir)) {
-  fs.mkdirSync(pagesDir);
-}
-//
-
-const convertMarkdownToTsx = (markdownFile: string) => {
-  const markdownContent = fs.readFileSync(markdownFile, 'utf-8');
-  const tsxContent = JSON.stringify(markdownContent);
-
-  if (path.basename(markdownFile) === 'README.md') {
-    return;
-  }
-  
-  const tsxFile = path.resolve(pagesDir, path.basename(markdownFile, '.md') + '.tsx');
-  fs.writeFileSync(
-    tsxFile,
-    `
-    import React from 'react';
-    import Markdown from 'markdown-to-jsx';
-
-    const markdown = ${tsxContent};
-
-    const Page: React.FC = () => (
-      <div>
-        <Markdown>{markdown}</Markdown>
-      </div>
-    );
-
-    export default Page;
-    `
-  );
-};
-
-const watcher = chokidar.watch(docsDir, {
-  persistent: true,
-  ignoreInitial: true,
-  depth: 0,
-});
 
 export {
   Announcement,
@@ -66,5 +21,3 @@ export {
   Card,
 };
 
-watcher.on('add', convertMarkdownToTsx);
-watcher.on('change', convertMarkdownToTsx);
