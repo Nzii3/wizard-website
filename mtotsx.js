3c83d685d83b95b9711fdbe59efeccc2e4ef01ee
Nziie3 <106406461+Nzii3@users.noreply.github.com>
2023-07-16 12:49:48 -0700

    Update mtotsx.js

--- mtotsx.js
+++ mtotsx.js
@@ -1,67 +1,72 @@
 const chokidar = require('chokidar');
 const fs = require('fs');
 const path = require('path');
-const mdToTsx = require('markdown-to-tsx');
+const mdToTsx = require('markdown-to-jsx');
 const { mkdirpSync } = require('fs-extra');
 
-const docsFolder = './docs';
-const pagesFolder = './pages';
-const componentsFolder = '../components';
+const docsDir = path.resolve(__dirname, './docs');
+const pagesDir = path.resolve(__dirname, './pages');
 
-const convertMarkdownToTsx = async (inputPath, outputPath) => {
+// just an extra check
+if (!fs.existsSync(pagesDir)) {
+  fs.mkdirSync(pagesDir);
+}
+//
+
+const convertMarkdownToTsx = async (inputPath) => {
   const stats = fs.statSync(inputPath);
 
   if (stats.isFile() && path.extname(inputPath) === '.md') {
     const markdownContent = fs.readFileSync(inputPath, 'utf-8');
     const tsxContent = await mdToTsx(markdownContent);
 
-    const tsxFilePath = outputPath + '.tsx';
+    const relativePath = path.relative(docsDir, inputPath);
+    const outputFolderPath = path.join(pagesDir, path.dirname(relativePath));
+    mkdirpSync(outputFolderPath);
+
+    const tsxFile = path.join(outputFolderPath, path.basename(inputPath, '.md') + '.tsx');
     fs.writeFileSync(
-      tsxFilePath,
+      tsxFile,
       `
-        import React from 'react';
-        import Markdown from 'markdown-to-jsx';
-        import { Navbar, Footer } from '${componentsFolder}';
+      import React from 'react';
+      import Markdown from 'markdown-to-jsx';
+      import { Navbar, Footer } from "../../components";
 
-        const markdown = ${JSON.stringify(tsxContent)};
+      const markdown = ${JSON.stringify(tsxContent)};
 
-        // CustomLink component to render links with a CSS class
-        const CustomLink = ({ children, ...props }) => (
-          <a className="markdown-links" {...props}>
-            {children}
-          </a>
-        );
+      // CustomLink component to render links with a CSS class
+      const CustomLink = ({ children, ...props }) => (
+        <a className="markdown-links" {...props}>
+          {children}
+        </a>
+      );
 
-        const Page: React.FC = () => (
-          <div className="grid min-h-screen gap-4">
-            <Navbar />
-            <div className="prose prose-invert rounded-xl bg-black/20 p-8 dark-background shadow" style={{placeSelf: 'center'}}>
-              <Markdown options={{ overrides: { a: { component: CustomLink } } }}>{markdown}</Markdown>
-            </div>
-            <Footer />
+      const Page: React.FC = () => (
+        <div className="grid min-h-screen gap-4">
+          <Navbar />
+          <div className="prose prose-invert rounded-xl bg-black/20 p-8 dark-background shadow" style={{placeSelf: 'center'}}>
+            <Markdown options={{ overrides: { a: { component: CustomLink } } }}>{markdown}</Markdown>
           </div>
-        );
+          <Footer />
+        </div>
+      );
 
-        export default Page;
+      export default Page;
       `
     );
   } else if (stats.isDirectory()) {
-    const folderName = path.basename(inputPath);
-    const outputFolderPath = path.join(outputPath, folderName);
-    mkdirpSync(outputFolderPath);
-
     const files = fs.readdirSync(inputPath);
     files.forEach((file) => {
       const filePath = path.join(inputPath, file);
-      const outputFile = path.join(outputFolderPath, file);
-      convertMarkdownToTsx(filePath, outputFile);
+      convertMarkdownToTsx(filePath);
     });
   }
 };
 
-convertMarkdownToTsx(docsFolder, pagesFolder);
+// handle stuff at startup
+convertMarkdownToTsx(docsDir);
 
-const watcher = chokidar.watch(docsFolder, {
+const watcher = chokidar.watch(docsDir, {
   persistent: true,
   ignoreInitial: false,
   depth: 0,
